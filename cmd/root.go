7f69c5e379ec5400f08b02612a74338ce7b9e394
vijayvenkatj <vijayvenkatj@gmail.com>
2026-01-12 17:06:37 +0530

    chore(cli): initialize Cobra-based CLI

---
+++ cmd/root.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "envcrypt",
+	Short: "Zero-trust, end-to-end encrypted environment variable management.",
+	Long: `EnvCrypt is a zero-trust environment variable manager.
+All secrets are encrypted and decrypted client-side, with immutable versioning, diffs, and rollbacks.
+EnvCrypt enables secure collaboration and CI/CD secret injection without server-side decryption.`,
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.envcrypt-cli.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
